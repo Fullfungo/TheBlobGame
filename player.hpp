5a257638e2fbc1cab86f9ea1e3de0875603f21c3
AbramovMA <m.abramov@student.vu.nl>
2022-04-10 03:58:02 +0200

    Your strategy goes here

--- player.hpp
+++ player.hpp
@@ -2,12 +2,16 @@
 #define PLAYER_H
 
 #include "game_meta.hpp"
-#include "cmath"
 
 
 class Player{
     public:
         PlayerChoice choose(size_t heads [[maybe_unused]], size_t tails [[maybe_unused]], size_t coins_left [[maybe_unused]] = 100) const{
+            /**************************************
+             *                                    *
+             *   Your player strategy goes here   *
+             *                                    *
+             **************************************/
             return PlayerChoice::Unsure;
         }
 };
